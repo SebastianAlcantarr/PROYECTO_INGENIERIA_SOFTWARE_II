c26ebab9617ded00c29501a5539a3983164a454c
Sebastian <sebastianalcantar323@gmail.com>
2025-11-12 21:28:37 -0700

    tema2

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -136,6 +136,13 @@
             Ir a Tema 1
           </router-link>
         </div>
+
+        <div></div>
+        <router-link to="/tema2"
+                     class="btn px-4 py-2 rounded bg-blue-600 text-white h-10 m-10 "
+                     >
+          ir al tema 2
+        </router-link>
       </main>
     </div>
   </div>
